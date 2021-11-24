ea88d829ae1bacb796bfde4b5e5db69ceee195a0
Sutin <poly.dev2018@gmail.com>
2021-11-25 02:41:43 +0700

    added storybook cjs

---
+++ windi.config.js
@@ -0,0 +1,13 @@
+export default {
+  shortcuts: {
+    'btn': {
+      'color': 'white',
+      '@apply': 'py-2 px-4 font-semibold rounded-lg',
+      '&:hover': {
+        '@apply': 'bg-green-700',
+        'color': 'black',
+      },
+    },
+    'btn-green': 'text-white bg-green-500 hover:bg-green-700',
+  },
+}
